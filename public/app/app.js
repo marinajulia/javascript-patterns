32ba9d56d65cb853fa2dd7a6dbd627ed231f4388
marinajulia <marinajulia707@gmail.com>
2022-02-21 22:11:42 -0300

    chore: log

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-import { handleStatus } from './utils/promise-helpers.js'
+import { handleStatus, log } from './utils/promise-helpers.js'
 
 document
     .querySelector('#myButton')
@@ -6,11 +6,7 @@ document
         fetch('http://localhost:3000/notas')
             .then(handleStatus)
             .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
-            .then(itens => {
-                console.log(itens);
-                return itens;
-            })
             .then(itens => itens.filter(item => item.codigo == '2143'))
             .then(itens => itens.reduce((total, item) => total + item.valor, 0))
-            .then(console.log)
-            .catch(console.log);
+            .then(log)
+            .catch(log);
