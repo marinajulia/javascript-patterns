6956d884003e7bd433ead0155d237aea7e74ffb5
marinajulia <marinajulia707@gmail.com>
2022-02-21 22:26:15 -0300

    chore: using flatMap

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,14 @@
 import { handleStatus, log } from './utils/promise-helpers.js'
-
+import './utils/array-helpers.js';
 document
     .querySelector('#myButton')
     .onclick = () =>
         fetch('http://localhost:3000/notas')
             .then(handleStatus)
-            .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
-            .then(itens => itens.filter(item => item.codigo == '2143'))
-            .then(itens => itens.reduce((total, item) => total + item.valor, 0))
+            .then(notas => 
+                notas.$flatMap(nota => nota.itens)
+                .filter(item => item.codigo == '2143')
+                .reduce((total, item) => total + item.valor, 0)
+            )
             .then(log)
             .catch(log);
