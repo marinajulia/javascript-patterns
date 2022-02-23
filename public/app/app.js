48fab1bb62a34b11c60f4283d82258d27fc649a9
marinajulia <marinajulia707@gmail.com>
2022-02-22 21:27:39 -0300

    chore: use bind

--- public/app/app.js
+++ public/app/app.js
@@ -2,6 +2,12 @@ import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
 
+// const ehDivisivel = (divisor, numero) => ! (numero % divisor);
+// const ehDivisivelPorDois = ehDivisivel.bind(null,2);
+
+// console.log(ehDivisivelPorDois(10));
+
+
 document
     .querySelector('#myButton')
     .onclick = () =>
