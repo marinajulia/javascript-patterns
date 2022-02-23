cf2a17e2f122e79da4c7820608571d99380034c7
marinajulia <marinajulia707@gmail.com>
2022-02-22 21:50:12 -0300

    chore: use reduce and reduceRight

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,12 +1,11 @@
 import { handleStatus } from '../utils/promise-helpers.js';
-
+import { partialize, compose, pipe } from '../utils/operators.js';
 const API = 'http://localhost:3000/notas';
 
 const getItensFromNotas = notas => notas.$flatMap(nota => nota.itens);
 const filterItemsByCode = (code, items) => items.filter(item => item.codigo == code);
+
 const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
-
-
 
 const sumItems = code => notas => notas
     .$flatMap(nota => nota.itens)
@@ -15,9 +14,16 @@ const sumItems = code => notas => notas
 
 export const notasService = {
     listAll() {
-        return fetch(API).then(handleStatus);
+        return fetch(API).then(handleStatus)
+        .catch(err =>{
+            console.log(err);
+            return Promise.reject('Não foi possível obter as notas fiscais')
+        });
     },
     sumItems(code) {
-        return this.listAll().then(sumItems(code));
+        const filterItems = partialize(filterItemsByCode, code);
+        const sumItems = pipe(getItensFromNotas, filterItems, sumItemsValue);
+        return this.listAll()
+            .then(sumItems);
     }
 }
---
+++ public/app/utils/operators.js
@@ -0,0 +1,7 @@
+export const partialize = (fn, ...args) => fn.bind(null, ...args);
+//fazendo da direita para a esquerda
+export const compose = (...fns) => value => 
+fns.reduceRight((previousValue, fn) => fn(previousValue), value)
+//fazendo da esquerda para a direita
+export const pipe = (...fns) => value => 
+fns.reduce((previousValue, fn) => fn(previousValue), value)
