d6fefeeb8a20ca023aed0736cdc0c1f1dda60835
marinajulia <marinajulia707@gmail.com>
2022-03-06 18:22:17 -0300

    chore: initialize takeUntil

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,18 @@
 import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
-
+import {takeUntil} from './utils/operators.js'
 // const ehDivisivel = (divisor, numero) => ! (numero % divisor);
 // const ehDivisivelPorDois = ehDivisivel.bind(null,2);
 
 // console.log(ehDivisivelPorDois(10));
 
+const showMessage = () =>console.log("oi");
+//3 vezes chamar a função showMessage:
+const operation = takeUntil(3, showMessage());
+
+let counter = 10;
+while(counter--) operation();
 
 document
     .querySelector('#myButton')
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -1,7 +1,12 @@
 export const partialize = (fn, ...args) => fn.bind(null, ...args);
 //fazendo da direita para a esquerda
-export const compose = (...fns) => value => 
-fns.reduceRight((previousValue, fn) => fn(previousValue), value)
+export const compose = (...fns) => value =>
+    fns.reduceRight((previousValue, fn) => fn(previousValue), value)
 //fazendo da esquerda para a direita
-export const pipe = (...fns) => value => 
-fns.reduce((previousValue, fn) => fn(previousValue), value)
+export const pipe = (...fns) => value =>
+    fns.reduce((previousValue, fn) => fn(previousValue), value)
+
+export const takeUntil = (times, fn) =>
+    () => {
+        if(times-- > 0) fn();
+    }
